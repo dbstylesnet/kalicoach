2c50a55a538caf6de842918bc79414b710a79732
Dariusz <dariusz.buczynski@nexio.pl>
2024-10-17 21:25:33 +0200

    CSS

--- app/index.js
+++ app/index.js
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import React from "react";
+import styled from "styled-components";
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>First screen</Text>
+        <View style={styles.container}>
+            <Text>asd sFirst screen 2</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "red",
+        justifyContent: "center",
+        width: '200px',
+        height: '100px',
+    }    
+});
+
+// const Container = styled.View`
+//     flex: 1;
+//     background: red;    
+
+// `;
 
 export default HomeScreen;
